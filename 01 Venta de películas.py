ed796fa81420fc4e3ba04edfb4939252cdf1c54d
Juan-C13 <clavijuan.eg@gmail.com>
2023-03-11 16:31:45 -0500

    Initialize

---
+++ 01 Venta de películas.py
@@ -0,0 +1,11 @@
+print("")
+C=int(input("Bienvenid@, ¿Cuántas películas desea comprar?:"))
+TP=C*65000
+IR=TP*0.16
+GN=TP-IR
+print("")
+print("DATOS DE LA VENTA")
+print("")
+print("Total a pagar:",TP)
+print("IVA:",IR)
+print("Ganancia neta:",GN)
